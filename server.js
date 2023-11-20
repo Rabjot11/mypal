af4b4f76a48fc30316924f88278d3fa39cff8604
Rabjot Singh <singhrabjot738@gmail.com>
2023-11-20 22:16:14 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import adoptRoutes from './routes/adoptRoutes.js';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 dotenv.config();
 
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
