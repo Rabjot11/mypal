0d891d6dd38bafb103469d2f5e325e3d750c0746
Rabjot Singh <singhrabjot738@gmail.com>
2023-11-21 20:22:31 +0530

    update

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ dotenv.config();
 
 connectDB();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
